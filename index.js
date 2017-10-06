7972d8a2dfc15a857a853e88ca7203f895fc5dcb
Igor Elias <igor.elias.muniz@gmail.com>
2017-10-06 02:25:27 -0300

    hot replace plugin

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('test')
+document.getElementById('send').onclick = () => {
+    const m = document.getElementById('message').value
+    document.getElementById('messages').innerHTML += `<li>${m}`
+}
+
+if (module && module.hot) {
+    module.hot.accept()
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack')
 
 const config = {
     entry: './index.js',
@@ -15,6 +16,13 @@ const config = {
 if ( process.env.NODE_ENV === 'development' ) {
     config.watch = true
     config.devtool = 'source-map'
+
+} else if ( process.env.NODE_ENV === 'hot' ) {
+    config.devtool = 'source-map'
+    config.devServer.hot = true
+    config.plugins = [
+        new webpack.HotModuleReplacementPlugin(),
+    ]
 }
 
 module.exports = config
